a6eae61483616d5c21f22867c20ceb02202383d4
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-11-07 15:19:30 +0200

    install RHF, create simple form skeleton, add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { useForm } from "react-hook-form";
+
 const App = () => {
-  return <div className="bg-green-700">test</div>;
+  const { register } = useForm();
+
+  return (
+    <div className="bg-white flex flex-col gap-8 p-6 rounded-2xl text-[#2A4144]">
+      <h1 className="text-[2rem] font-bold leading-[100%] tracking-[-1px]">
+        Contact Us
+      </h1>
+      <form>
+        <div className="flex flex-col">
+          <label>
+            First Name <span>*</span>
+          </label>
+          <input type="text" {...register("firstName")} />
+        </div>
+        <div>
+          <label>
+            Last Name <span>*</span>
+          </label>
+          <input type="text" {...register("lastName")} />
+        </div>
+        <div>
+          <label>
+            Email Address <span>*</span>
+          </label>
+          <input type="email" {...register("email")} />
+        </div>
+        <input type="text" />
+        <label>
+          Query Type <span>*</span>
+        </label>
+        <input type="radio" />
+        <input type="radio" />
+        <textarea name="" id=""></textarea>
+        <input type="checkbox" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
